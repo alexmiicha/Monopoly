b02dc9755d0144873fc3090fcf9cf1f7b5f73579
alexandre michalik <alexandremichalik@MPB-alexandre.local>
2021-10-21 16:07:29 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module monopoly.monopoly {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens monopoly.monopoly to javafx.fxml;
+    exports monopoly.monopoly;
+}
